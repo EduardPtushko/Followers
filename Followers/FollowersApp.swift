8d1368189d8bba6c6dfc95ce0728aa35fbf14731
Эдуард Птушко <ptushko@gmail.com>
2024-01-13 09:58:18 +0300

    Initial Commit

---
+++ Followers/FollowersApp.swift
@@ -0,0 +1,17 @@
+//
+//  FollowersApp.swift
+//  Followers
+//
+//  Created by Eduard Ptushko on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FollowersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
