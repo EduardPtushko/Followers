98a41113d18b5d016571fc893214af511b7dd90f
Эдуард Птушко <ptushko@gmail.com>
2024-02-21 13:32:14 +0300

    Add AvatarImageView with cache

--- Followers/Custom Views/AvatarImageView.swift
+++ Followers/Custom Views/AvatarImageView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct AvatarImageView: View {
+    let cache = NetworkManager.shared.cache
+    @State private var image: Image = Image(.avatarPlaceholder)
+    let urlSting: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        image
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .task {
+                await getAvatarImage(from: urlSting)
+            }
+    }
+
+    func getAvatarImage(from urlString: String) async {
+        let cacheKey = NSString(string: urlString)
+
+        if let image = cache.object(forKey: cacheKey) {
+            self.image = Image(uiImage: image)
+            return
+        }
+        guard let url = URL(string: urlString) else { return }
+
+        guard  let (data, response) = try? await URLSession.shared.data(from: url) as? (Data, HTTPURLResponse), response.statusCode == 200 else {
+            return
+        }
+
+        guard let uiImage = UIImage(data: data) else { return }
+
+        cache.setObject(uiImage, forKey: cacheKey)
+        image = Image(uiImage: uiImage)
     }
 }
 
 #Preview {
-    AvatarImageView()
+    AvatarImageView(urlSting: "djfjf")
 }
